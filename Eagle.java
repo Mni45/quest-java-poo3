173e4e5ffae093b94073e0440b35e58478cad896
Amani Berkani <amaniberkani@icloud.com>
2019-10-01 22:44:14 +0200

    J'ai enfin compris

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -17,8 +17,38 @@ public class Eagle extends Bird {
         return flying;
     }
 
-    @Override
+  
     public String sing() {
         return "Screech!";
     }
+   
+    public void takeOff() {
+	this.flying=true;
+        System.out.println(this.getName() + " takes off in the Sky.");
+    }
+
+    
+    public int ascend(int meters) {
+	this.altitude=this.altitude+meters;
+        System.out.println(this.getName() + " flies uppward "+ meters+ "m.");
+	return this.altitude ;
+    }
+
+
+    
+    public int descend(int meters) {
+	this.altitude=this.altitude - meters;
+	System.out.println("It flies downward " + meters+"m.");
+        return this.altitude ;
+    }
+
+    
+    public void land() {
+	if(this.altitude>1){
+       		System.out.println("It can't land.");
+	} else {
+		System.out.println("It can land.");
+	}
+    }
+
 }
